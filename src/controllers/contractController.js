3f13f4e323d8a6b6e192238d4e268c31d0e3e35a
Алексей Богачёв <aleksejbogacev@Aleksejs-MacBook-Pro.local>
2025-04-01 23:25:47 +0300

    Добавление функционала

--- src/controllers/contractController.js
+++ src/controllers/contractController.js
@@ -3,7 +3,6 @@ const Contract = require('../models/Contract');
 const Employee = require('../models/Employee');
 const Application = require('../models/Application');
 const StatusContract = require('../models/StatusContract');
-const User = require('../models/User');
 const Tariff = require('../models/Tariff');
 
 const getContracts = async (req, res) => {
@@ -36,28 +35,77 @@ const getContracts = async (req, res) => {
             ]
         });
 
-        for (let contract of contracts) {
-            const { id_user } = contract.application;
-            const [user] = await sequelize.query(
-                `SELECT * FROM decrypted_users WHERE id_user = :id_user`, 
-                { replacements: { id_user }, type: sequelize.QueryTypes.SELECT }
-            );
-
-            if (user) {
-                contract.application.dataValues.user = {
-                    phone_number: user.phone_number,
-                    email: user.email
+        const userIds = contracts.map(contract => contract.application.id_user);
+        
+        const usersData = await sequelize.query(
+            `SELECT 
+                u.id_user, u.phone_number, u.email,
+                np.surname, np.name, np.patronymic, np.date_of_birth, np.gender,
+                np.registration_address, np.residential_address,
+                np.passport_number, np.passport_series,
+                le.name AS company_name, le.tin, le.registration_number, 
+                le.director_full_name, le.contact_person, le.contact_phone, 
+                le.actual_address, le.website
+            FROM decrypted_users u
+            LEFT JOIN decrypted_natural_persons np ON u.id_user = np.id_user
+            LEFT JOIN legal_entity le ON u.id_user = le.id_user
+            WHERE u.id_user IN (:userIds)`,
+            { replacements: { userIds }, type: sequelize.QueryTypes.SELECT }
+        );
+        
+        const usersMap = {};
+        usersData.forEach(user => {
+            let userType = "Неизвестно";
+            let userDetails = {};
+            
+            if (user.surname) {
+                userType = "Физическое лицо";
+                userDetails = {
+                    surname: user.surname,
+                    name: user.name,
+                    patronymic: user.patronymic,
+                    date_of_birth: user.date_of_birth,
+                    gender: user.gender,
+                    registration_address: user.registration_address,
+                    residential_address: user.residential_address,
+                    passport_number: user.passport_number,
+                    passport_series: user.passport_series
                 };
-            } else {
-                contract.application.dataValues.user = null;
             }
-        }
+            
+            if (user.company_name) {
+                userType = "Юридическое лицо";
+                userDetails = {
+                    company_name: user.company_name,
+                    tin: user.tin,
+                    registration_number: user.registration_number,
+                    director_full_name: user.director_full_name,
+                    contact_person: user.contact_person,
+                    contact_phone: user.contact_phone,
+                    actual_address: user.actual_address,
+                    website: user.website
+                };
+            }
+
+            usersMap[user.id_user] = {
+                phone_number: user.phone_number,
+                email: user.email,
+                user_type: userType,
+                ...userDetails
+            };
+        });
+
+        contracts.forEach(contract => {
+            contract.application.dataValues.user = usersMap[contract.application.id_user] || null;
+        });
 
         res.json(contracts);
     } catch (err) {
         res.status(500).json({ error: 'Ошибка при получении данных: ' + err.message });
     }
 };
+
+module.exports = { getContracts };
 
 const getContractById = async (req, res) => {
     const { id } = req.params;
